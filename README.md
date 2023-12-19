77726ea4faa0cfec5afce99dfa7b5c4542d59099
Sofia Rodrigues <algebraic.sofia+github@gmail.com>
2023-12-19 12:58:10 -0300

    chore: add example

--- README.md
+++ README.md
@@ -20,3 +20,20 @@ Add Http to your `lakefile.lean`:
 ```lean
 require Specs from git "https://github.com/axiomed/Specs.lean.git"
 ```
+
+This is a simple test suite for numbers:
+
+```lean
+import Specs
+
+open Specs
+open Specs.Matchers
+
+def main := runCli do
+  describe "check if numbers are different" do
+    it "numbers greater than 0 but less than 10" do
+      isEqual 1 1
+      isGreater 3 1
+    it "numbers greater than 10" do
+      isGreater 20 15
+```
--- Tests.lean
+++ Tests.lean
@@ -4,17 +4,9 @@ open Specs
 open Specs.Matchers
 
 def main := runCli do
-  describe "test" do
-    it "test" do
-      assert "eq" 1 1
-      assert "eq" 1 2
-
-  describe "test" do
-    it "test" do
-      assert "eq" 1 1
-      assert "eq" 1 2
-
-  describe "test" do
-    it "test" do
-      assert "eq" 1 1
-      assert "eq" 1 2
+  describe "check if numbers are different" do
+    it "numbers greater than 0 but less than 10" do
+      isEqual 1 1
+      isGreater 3 1
+    it "numbers greater than 10" do
+      isGreater 20 15
